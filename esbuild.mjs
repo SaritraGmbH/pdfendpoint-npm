77fe0f2251065bbd610f57d23a058c0de1e63712
Dmitry Dyachkov <git@e965.ru>
2023-03-27 14:49:39 +0500

    chore: new build flow

---
+++ esbuild.mjs
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import glob from 'glob';
+import * as esbuild from 'esbuild';
+
+await fs.rm('dist', { recursive: true, force: true });
+
+const sourceFiles = await glob('src/**/*.ts');
+
+await esbuild.build({
+	entryPoints: sourceFiles,
+	outdir: 'dist/esm',
+	format: 'esm',
+	outExtension: { '.js': '.mjs' },
+});
+
+await esbuild.build({
+	entryPoints: sourceFiles,
+	outdir: 'dist/cjs',
+	format: 'cjs',
+	outExtension: { '.js': '.cjs' },
+});
