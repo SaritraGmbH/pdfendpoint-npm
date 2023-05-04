ad14f630474637107224cfd6ee5983fd6f2ed705
Dmitry Dyachkov <git@e965.ru>
2023-05-04 14:10:13 +0500

    feat: urls as constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const PDFENDPOINT_API_HOST = 'https://api.pdfendpoint.com';
 
-export const PDFENDPOINT_CONVERT_URL = PDFENDPOINT_API_HOST + '/v1/convert';
+export const PDFENDPOINT_CONVERT_URL = `${PDFENDPOINT_API_HOST}/v1/convert` as const;
 
-export const PDFENDPOINT_USAGE_URL = PDFENDPOINT_API_HOST + '/v1/usage';
+export const PDFENDPOINT_USAGE_URL = `${PDFENDPOINT_API_HOST}/v1/usage` as const;
 
-export const PDFENDPOINT_LIST_URL = PDFENDPOINT_API_HOST + '/v1/list';
+export const PDFENDPOINT_LIST_URL = `${PDFENDPOINT_API_HOST}/v1/list` as const;
 
-export const PDFENDPOINT_HEALTH_URL = PDFENDPOINT_API_HOST + '/health';
+export const PDFENDPOINT_HEALTH_URL = `${PDFENDPOINT_API_HOST}/health` as const;
