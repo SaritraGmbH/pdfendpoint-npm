22873f6b36001ff8689c8b34e9cbed155003981b
Dmitry Dyachkov <git@e965.ru>
2024-09-06 12:17:45 +0500

    chore: upgrade project dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint', 'prettier'],
 	rules: {
+		'@typescript-eslint/no-explicit-any': ['off'],
 		'prettier/prettier': ['warn', { endOfLine: 'auto' }],
 	},
 };
